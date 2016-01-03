fb2dee20723820ed26dd92451a0a561206b00a02
Filip Nyquist <fillerix@fillerix.se>
2016-01-03 23:22:53 +0100

    Moved from gist to repo

---
+++ install.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# Script to download and install opls script automatically
+echo "Installing dependencies"
+sudo apt-get update
+sudo apt-get install nodejs
+sudo apt-get install zip
+wget https://github.com/opl-/winter-arg-tester/archive/master.zip
+unzip master.zip
+rm master.zip
+echo "Done downloading, go into winter-arg-tester and type: nodejs cli.js bot"
