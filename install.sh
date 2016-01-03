dd17316f66b20db57343062963e174fa4591cc23
Filip Nyquist <fillerix@fillerix.se>
2016-01-03 23:23:34 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -7,4 +7,4 @@ sudo apt-get install zip
 wget https://github.com/opl-/winter-arg-tester/archive/master.zip
 unzip master.zip
 rm master.zip
-echo "Done downloading, go into winter-arg-tester and type: nodejs cli.js bot"
+echo "Done downloading, go into winter-arg-tester and type: nodejs cli.js bot or run the scripts!"
